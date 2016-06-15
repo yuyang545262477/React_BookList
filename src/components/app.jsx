71602e964c8e0604f4c26081d7025b0acdaa255a
David_Yu <yuyang545262477@gmail.com>
2016-06-15 20:52:15 +0800

    finish the second time codding.

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BookList from "../container/book-list";
+import BookDetail from "../container/book-detail";
+
+class APP extends React.Component {
+    render() {
+        return (
+            <div>
+                <BookList />
+                <BookDetail />
+            </div>
+        )
+    };
+}
+
+export default APP;
